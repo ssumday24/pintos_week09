7bef1291ef41285e87e8bdcf9e5eb2d9d775a51d
sunha20 <c.seonha20@gmail.com>
2025-07-29 00:40:15 +0900

    fix: file system 동기화

--- pintos/userprog/syscall.c
+++ pintos/userprog/syscall.c
@@ -57,6 +57,9 @@ void close(int fd);
 #define MSR_LSTAR 0xc0000082        /* Long mode SYSCALL target */
 #define MSR_SYSCALL_MASK 0xc0000084 /* Mask for the eflags */
 
+/* ====== lock ====== */
+struct lock filesys_lock;
+
 void syscall_init(void) {
     write_msr(MSR_STAR, ((uint64_t)SEL_UCSEG - 0x10) << 48 | ((uint64_t)SEL_KCSEG) << 32);
     write_msr(MSR_LSTAR, (uint64_t)syscall_entry);
@@ -65,6 +68,9 @@ void syscall_init(void) {
      * until the syscall_entry swaps the userland stack to the kernel
      * mode stack. Therefore, we masked the FLAG_FL. */
     write_msr(MSR_SYSCALL_MASK, FLAG_IF | FLAG_TF | FLAG_DF | FLAG_IOPL | FLAG_AC | FLAG_NT);
+
+    // lock init
+    lock_init(&filesys_lock);
 }
 
 /* ====== 메인 시스템콜 인터페이스  => 커널공간 ===== */
@@ -224,23 +230,32 @@ int wait(tid_t pid) {  // Case : 4
 }
 
 bool create(const char *file, unsigned initial_size) {  // Case : 5
+    bool success;
     if (!check_address(file)) {
         exit(-1);
     }
     // filesys.c 에 정의된 함수 사용
-    return filesys_create(file, initial_size);
+    lock_acquire(&filesys_lock);
+    success = filesys_create(file, initial_size);
+    lock_release(&filesys_lock);
+
+    return success;
 }
 
 bool remove(const char *file) {  // Case : 6 -> 일단 간단한 버전
+    bool success;
     if (!check_address(file)) {
         exit(-1);
     }
     // filesys.c 에 정의된 함수 사용
-    return filesys_remove(file);
+    lock_acquire(&filesys_lock);
+    success = filesys_remove(file);
+    lock_release(&filesys_lock);
+
+    return success;
 }
 
 int open(const char *file_name) {  // Case : 7
-
     if (!check_address(file_name)) {
         exit(-1);  // 유효하지 않은 파일이면 exit
     }
@@ -255,7 +270,9 @@ int open(const char *file_name) {  // Case : 7
     strlcpy(file_name_copy, file_name, PGSIZE);
 
     // 파일 열기
+    lock_acquire(&filesys_lock);
     struct file *file_ptr = filesys_open(file_name_copy);
+    lock_release(&filesys_lock);
     palloc_free_page(file_name_copy);
 
     if (file_ptr == NULL) {
@@ -277,7 +294,7 @@ int open(const char *file_name) {  // Case : 7
         }
     }
 
-    // FD 테이블의 최대번호는 몇으로 해야할까?
+    // FD 할당
     for (int i = 2; i < FDT_MAX_SIZE; i++) {
         if ((cur->fdt)[i] == NULL)  // fd 인덱스가 비어있다면
         {
@@ -307,7 +324,9 @@ int filesize(int fd) {  // Case : 8 -> read()에서 호출
     }
 
     // file.c
+    lock_acquire(&filesys_lock);
     size = file_length(cur_file);
+    lock_release(&filesys_lock);
 
     return size;
 }
@@ -360,13 +379,13 @@ int read(int fd, void *buffer, unsigned size) {  // Case : 9
         }
 
         // 동시접근 막기위한 락 획득
-        // lock_acquire(&);
+        lock_acquire(&filesys_lock);
 
         // file.c 의 file_read 사용
         bytes_read = file_read(cur_file, buffer, size);
 
         // 락 해제
-        // lock_release(&);
+        lock_release(&filesys_lock);
     }
 
     return bytes_read;
@@ -414,8 +433,10 @@ int write(int fd, const void *buffer, unsigned size) {  // Case : 10
             return -1;
         }
 
+        lock_acquire(&filesys_lock);
         // file_write는 파일 끝까지만 쓰고 실제 쓰여진 바이트 수를 반환
         bytes_written = file_write(file_obj, buffer, size);
+        lock_release(&filesys_lock);
     }
     return bytes_written;
 }
