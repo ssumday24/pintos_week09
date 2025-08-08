a039c266f881d5d68b827be6c8ec6d638730efa9
송상록 <drinkingstraw33@gmail.com>
2025-08-08 20:25:41 +0900

    mmap, do_mmap 구현

--- pintos/vm/file.c
+++ pintos/vm/file.c
@@ -1,6 +1,16 @@
 /* file.c: Implementation of memory backed file object (mmaped object). */
 
 #include "vm/vm.h"
+#include "userprog/process.h"
+#include "threads/vaddr.h"
+
+struct mmap_file{
+    void *addr;         // 시작 페이지의 주소
+    struct file *file;  // 파일 주소
+    struct list_elem elem;   // struct thread의 mmap_list 삽입용도
+    size_t length;      // 매핑된 영역 크기
+    size_t ofs;         // offset
+};
 
 static bool file_backed_swap_in(struct page *page, void *kva);
 static bool file_backed_swap_out(struct page *page);
@@ -42,7 +52,48 @@ static void file_backed_destroy(struct page *page) {
 }
 
 /* Do the mmap */
-void *do_mmap(void *addr, size_t length, int writable, struct file *file, off_t offset) {}
+void *do_mmap(void *addr, size_t length, int writable, struct file *file, off_t ofs) {
+    file = file_reopen(file);
+    if (file == NULL){
+        return NULL;
+    }
+
+    size_t read_bytes = file_length(file);
+    void *upage = addr;
+
+    // 반복해서 페이지할당 요청
+    while (read_bytes > 0) {
+        size_t page_read_bytes = read_bytes < PGSIZE ? read_bytes : PGSIZE;
+        size_t page_zero_bytes = PGSIZE - page_read_bytes;
+
+        struct file_aux *aux = malloc(sizeof(struct file_aux));
+        aux -> file = file;
+        aux -> ofs = ofs;
+        aux -> page_read_bytes = page_read_bytes;
+        aux -> page_zero_bytes = page_zero_bytes;
+
+        if (!vm_alloc_page_with_initializer(VM_FILE, upage, writable, lazy_load_segment, aux)){
+            free(aux);
+            return NULL;
+        }
+        
+        read_bytes -= page_read_bytes;
+        upage += PGSIZE;
+        ofs += page_read_bytes;
+    } 
+
+    // mmap_file 구조체 만들고 삽입
+    struct mmap_file *mm = malloc(sizeof(struct mmap_file));
+    mm -> addr = addr;
+    mm -> file = file;
+    mm -> ofs = ofs;
+    mm -> length = length;
+
+    list_push_back(&thread_current() -> mmap_list, &mm -> elem);
+
+    // 리스트 삽입 나중에
+    return addr;
+}
 
 /* Do the munmap */
 void do_munmap(void *addr) {}
